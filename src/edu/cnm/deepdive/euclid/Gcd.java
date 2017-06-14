a1a1344e1dc33ef37f78008216c79934c8b92123
User <User@User-PC.admin.ad.cnm.edu>
2017-06-14 09:50:14 -0600

    try catch statement additions

--- src/edu/cnm/deepdive/euclid/Gcd.java
+++ src/edu/cnm/deepdive/euclid/Gcd.java
@@ -4,12 +4,18 @@ public class Gcd {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    int a = Integer.parseInt(args [0]);
-    int b = Integer.parseInt(args [1]);
-    System.out.printf("a = %d, b = %d%n", a, b);
-        
-    
+    try {
+      int a = Integer.parseInt(args[0]);
+      int b = Integer.parseInt(args[1]);
+      System.out.printf("a = %d, b = %d%n", a, b);
+
+
+
+    } catch (NumberFormatException ex) {
+      ex.printStackTrace();
+      System.out.println("Must use integers only");
+
+    }
 
   }
-
 }
